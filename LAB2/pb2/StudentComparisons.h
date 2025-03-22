0159011071d58f5896a2a8fa0e5c47cb7a90a019
Georgiana <131597237+Letitia18@users.noreply.github.com>
2025-03-22 16:14:57 +0200

    Create StudentComparisons.h

---
+++ LAB2/pb2/StudentComparisons.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Student.h"
+
+int CompareByName(const Student& s1, const Student& s2);
+int CompareByMathGrade(const Student& s1, const Student& s2);
+int CompareByEnglishGrade(const Student& s1, const Student& s2);
+int CompareByHistoryGrade(const Student& s1, const Student& s2);
+int CompareByAverageGrade(const Student& s1, const Student& s2);
